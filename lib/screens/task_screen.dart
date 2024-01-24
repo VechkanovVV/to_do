b5b26261247834c7a36f2a8bff8c23bca8a8b958
VechkanovVV <hatavidit84@gmail.com>
2024-01-24 13:37:38 +0300

    change task_screen (created list of tasks) && create a blocs directory

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../modules/task.dart';
 
 class TaskScreen extends StatelessWidget {
   TaskScreen({Key? key}) : super(key: key);
 
-  List<Task> taskList = [];
+  List<Task> taskList = [
+    Task(title: 'task1'),
+    Task(title: 'task2'),
+    Task(title: 'task3'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +26,27 @@ class TaskScreen extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
+        children: [
           Center(
             child: Chip(
               label: Text(
                 'Tasks:',
               ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: taskList.length,
+              itemBuilder: (context, index) {
+                var currentTask = taskList[index];
+                return ListTile(
+                  title: Text(currentTask.title), // Исправлено: title должен быть виджетом Text
+                  trailing: Checkbox(
+                    value: currentTask.isDone,
+                    onChanged: (value) {},
+                  ),
+                );
+              },
             ),
           ),
         ],
