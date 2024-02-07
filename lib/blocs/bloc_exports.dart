c3fd9dfce55debe3418d374ef07469cd384871d1
Vladislav Vechkanov <v.vechkanov@innopolis.university>
2024-02-08 02:58:58 +0300

    change structere in the blocs

--- lib/blocs/bloc_exports.dart
+++ lib/blocs/bloc_exports.dart
@@ -1,4 +1,4 @@
 export './bloc/task_bloc.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:hydrated_bloc/hydrated_bloc.dart';
-export './bloc/editing_task_bloc.dart';
+export 'editing_task/editing_task_bloc.dart';
