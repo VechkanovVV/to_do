3565d5055f32d4c89991101a36bdd5d3ac3b379c
Vladislav Vechkanov <v.vechkanov@innopolis.university>
2024-02-18 21:23:18 +0300

    add ability to delete a task by swipe

--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -20,137 +20,146 @@ class _ListItemState extends State<ListItem> {
   @override
   Widget build(BuildContext context) {
     var currentTask = widget.tasks[widget.index];
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-        side: BorderSide(
-            color: (widget.tasks[widget.index].priority == PriorityState.high)
-                ? Colors.red
-                : (widget.tasks[widget.index].priority == PriorityState.low)
-                    ? Colors.green
-                    : (widget.tasks[widget.index].priority ==
-                            PriorityState.medium)
-                        ? Colors.yellow
-                        : Colors.black,
-            width: 3.0),
-      ),
-      child: AnimatedContainer(
-        height: (isExpanded) ? 115 : 55,
-        duration: const Duration(milliseconds: 250),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    const SizedBox(
-                      width: 3,
-                    ),
-                    IconButton(
-                        onPressed: () {
-                          setState(() {
-                            isExpanded = !isExpanded;
-                          });
-                        },
-                        icon: (isExpanded)
-                            ? const Icon(Icons.keyboard_arrow_up)
-                            : const Icon(Icons.keyboard_arrow_down)),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: Text(
-                        currentTask.title,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 27,
+    return Dismissible(
+      onDismissed: (_) {
+        context
+            .read<TaskBloc>()
+            .add(DeleteTask(task: widget.tasks[widget.index]));
+      },
+      key: ValueKey(widget.tasks[widget.index].title),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          side: BorderSide(
+              color: (widget.tasks[widget.index].priority == PriorityState.high)
+                  ? Colors.red
+                  : (widget.tasks[widget.index].priority == PriorityState.low)
+                      ? Colors.green
+                      : (widget.tasks[widget.index].priority ==
+                              PriorityState.medium)
+                          ? Colors.yellow
+                          : Colors.black,
+              width: 3.0),
+        ),
+        child: AnimatedContainer(
+          height: (isExpanded) ? 115 : 55,
+          duration: const Duration(milliseconds: 250),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isExpanded = !isExpanded;
+                            });
+                          },
+                          icon: (isExpanded)
+                              ? const Icon(Icons.keyboard_arrow_up)
+                              : const Icon(Icons.keyboard_arrow_down)),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: Text(
+                          currentTask.title,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 27,
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                  Row(children: [
+                    StarCheckBox(
+                      task: widget.tasks[widget.index],
                     ),
-                  ],
-                ),
-                Row(children: [
-                  StarCheckBox(
-                    task: widget.tasks[widget.index],
-                  ),
-                  Checkbox(
-                    value: currentTask.isDone,
-                    onChanged: (value) {
-                      context
-                          .read<TaskBloc>()
-                          .add(UpdateTask(task: widget.tasks[widget.index]));
-                    },
-                  ),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                ]),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    SizedBox(
-                      width: (isExpanded) ? 8 : 0,
+                    Checkbox(
+                      value: currentTask.isDone,
+                      onChanged: (value) {
+                        context
+                            .read<TaskBloc>()
+                            .add(UpdateTask(task: widget.tasks[widget.index]));
+                      },
                     ),
-                    AnimatedContainer(
-                      height: (isExpanded) ? 42 : 0,
-                      width: 130,
-                      duration: Duration(milliseconds: (isExpanded) ? 400 : 35),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.grey.shade200,
-                          ),
-                          onPressed: () {
-                            showDialog<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('Task\'s description'),
-                                  content: Text(
-                                      widget.tasks[widget.index].description),
-                                  actions: <Widget>[
-                                    ElevatedButton(
-                                      child: const Text('Ok'),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                          child: const Text(
-                            'Show\ndescription',
-                            style: TextStyle(color: Colors.black),
-                          )),
+                    const SizedBox(
+                      width: 4,
                     ),
-                  ],
-                ),
-                AnimatedContainer(
-                  height: (isExpanded) ? 30 : 0,
-                  duration: Duration(
-                    milliseconds: (isExpanded) ? 900 : 35,
+                  ]),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: (isExpanded) ? 8 : 0,
+                      ),
+                      AnimatedContainer(
+                        height: (isExpanded) ? 42 : 0,
+                        width: 130,
+                        duration:
+                            Duration(milliseconds: (isExpanded) ? 400 : 35),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.grey.shade200,
+                            ),
+                            onPressed: () {
+                              showDialog<void>(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Task\'s description'),
+                                    content: Text(
+                                        widget.tasks[widget.index].description),
+                                    actions: <Widget>[
+                                      ElevatedButton(
+                                        child: const Text('Ok'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: const Text(
+                              'Show\ndescription',
+                              style: TextStyle(color: Colors.black),
+                            )),
+                      ),
+                    ],
                   ),
-                  child: IconButton(
-                    icon: Icon(Icons.edit, size: (isExpanded) ? 25 : 0),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => EditScreen(
-                                  task: widget.tasks[widget.index])));
-                    },
+                  AnimatedContainer(
+                    height: (isExpanded) ? 30 : 0,
+                    duration: Duration(
+                      milliseconds: (isExpanded) ? 900 : 35,
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.edit, size: (isExpanded) ? 25 : 0),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EditScreen(
+                                    task: widget.tasks[widget.index])));
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
